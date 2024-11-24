6756daa2bec3c929cf08bf9ba834aa3c7924f3c3
Man-Chun Yu <brian0821yu@gmail.com>
2024-11-24 22:23:31 +0000

    Add authentication in template

--- template/apptemplate/app.py
+++ template/apptemplate/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, flash, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from auth import login_required
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.after_request
 def after_request(response):
@@ -13,3 +21,17 @@ def after_request(response):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        ...
+    else:
+        ...
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    ...
+        
